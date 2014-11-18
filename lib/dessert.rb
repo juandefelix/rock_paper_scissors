972aa380b75e532bf367cd121dabe05a6ce39c37
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2014-11-18 16:52:05 -0600

    initial commit

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,53 @@
 class Dessert
+
+  attr_accessor :name, :calories
+
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
-    # your code here
+    @calories < 200
   end
   def delicious?
-    # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @flavor = flavor
+    @name = "#{flavor} jelly bean"
+    @calories = 5
+  end
+
+  def delicious?
+    @flavor == "licorice" ? false : true
   end
 end
+
+
+
+# subject = Dessert.new('cake', 400)
+# p subject.calories == 400
+# p subject.name == 'cake'
+# p subject.delicious? == true
+# p subject.healthy? == false
+# puts
+# subject = Dessert.new('apple', 75)
+# p subject.delicious? == true
+# p subject.healthy? == true
+# puts
+# dessert = Dessert.new('xxx', 0)
+# dessert.calories = 80
+# p dessert.calories == 80
+# dessert.name = 'ice cream'
+# p dessert.name == 'ice cream'
+# puts
+# subject = JellyBean.new('vanilla')
+# p subject.calories == 5
+# p subject.name.match /vanilla jelly bean/i
+# p subject.delicious? == true
+# puts
+# subject = JellyBean.new('licorice')
+# p subject.delicious? == false
--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,14 +1,69 @@
+# require 'pry-byebug'
 class RockPaperScissors
 
   # Exceptions this class can raise:
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    check_players(player1, player2)
+
+    combination = player1[1] + player2[1]
+    return player1 if ['PP','RR','SS'].include? combination
+    ['RS', 'SP', 'PR'].include?(combination) ? player1 : player2
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    # binding.pry
+    # find a match
+    # play the match
+    return self.winner(tournament[0], tournament[1] ) if self.find_a_match(tournament)
+
+    self.tournament_winner([self.tournament_winner(tournament[0]), self.tournament_winner(tournament[1])])
+
+    # substitute the match for the winner
+    # do this until there's no more matches
   end
 
+
+
+
+
+private
+
+  def self.check_players(player1, player2)
+    strategies = [player1[1], player2[1]]
+    allowed_strategies = ['P','R','S']
+
+    raise(NoSuchStrategyError, "Strategy must be one of R,P,S") unless (strategies - allowed_strategies).length == 0
+  end
+
+  def self.find_a_match(tournament)
+    # binding.pry
+    tournament[0][0].class == String && tournament[1][0].class == String
+  end
 end
+
+rock = ['Armando','R'] ; paper = ['Dave','P'] ; scissors = ['Sam','S']
+
+tourney = [
+        [
+          [ ["Armando", "P"], ["Dave", "S"] ],      
+          [ ["Richard", "R"], ["Michael", "S"] ]
+        ],
+        [
+          [ ["Allen", "S"], ["Omer", "P"] ],
+          [ ["David E.", "R"], ["Richard X.", "P"] ]
+        ]
+      ]
+
+
+# p RockPaperScissors.winner(rock, scissors) == rock
+# p RockPaperScissors.winner(paper, scissors) == scissors
+# p RockPaperScissors.winner(rock, paper) == paper
+# p RockPaperScissors.winner(scissors, ['Dave','S']) == scissors
+# puts
+# p RockPaperScissors.tournament_winner([rock,paper]) == paper
+# puts
+# p RockPaperScissors.tournament_winner(tourney) == ['Richard', 'R']
+
+# RockPaperScissors.winner(rock, ['Dave', 'w'])
